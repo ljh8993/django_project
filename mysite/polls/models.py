836ac317d25fe7db2f24109e538eb72e66ab9dc8
jooha <dlwngk14@naver.com>
2020-05-26 01:53:16 +0900

    내일 다시

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,13 +9,25 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
 
 class Useritem(models.Model):
     user_email = models.CharField(max_length=254)
     item_title = models.CharField(max_length=200)
     item_contents = models.TextField()
     count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.item_title
